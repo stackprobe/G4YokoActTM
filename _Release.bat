a7e5d6f6cd1f97f72b122601bab53722ceafcf92
stackprobe <stackprobes@gmail.com>
2019-08-29 09:11:19 +0900

    Dream

--- _Release.bat
+++ _Release.bat
@@ -12,7 +12,7 @@ COPY /B GreenDiamond\GreenDiamond\bin\Release\DxLibDotNet.dll out
 C:\Factory\Tools\xcp.exe doc out
 C:\Factory\Tools\xcp.exe C:\Dev\Fairy\Donut2\doc out
 
-C:\Factory\SubTools\zip.exe /G out GreenDiamond
+C:\Factory\SubTools\zip.exe /PE- /G out GreenDiamond
 C:\Factory\Tools\summd5.exe /M out
 
 PAUSE
